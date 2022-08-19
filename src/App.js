28b43caa94e989ca485ef1b0b20db70645fc0f19
Anton Prokhorov <varpmen@gmail.com>
2022-08-19 18:11:23 +0300

    fix/develop branch

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { Route, Switch } from "react-router-dom";
-
-function App() {}
-<Switch>
-  <Route path="/sign-up"></Route>
-  <Route path="/sign-in"></Route>
-</Switch>;
-export default App;
