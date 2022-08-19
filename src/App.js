f8c6b6f077d3516fc0020230585f21e38c9ec7cf
Anton Prokhorov <varpmen@gmail.com>
2022-08-19 07:09:55 +0300

    feat/initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Route, Switch } from "react-router-dom";
+
+function App() {}
+<Switch>
+  <Route path="/sign-up"></Route>
+  <Route path="/sign-in"></Route>
+</Switch>;
+export default App;
