5173b77ac58685c571e2751c7ac8e11ebd480032
Anton Prokhorov <varpmen@gmail.com>
2022-08-19 18:15:05 +0300

    feat/ add route

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,8 @@
+import { Switch, Route } from "react-router-dom";
+
 function App() {}
+<Switch>
+  <Route path="/sign-in"></Route>
+  <Route path="/sign-up"></Route>
+</Switch>;
 export default App;
